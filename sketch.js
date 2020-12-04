a999c9bddbf715bd7b758177eb764693d05e2ec0
Sindhuja R <54587799+Sindhuja-R-21@users.noreply.github.com>
2020-12-04 18:09:18 +0530

    Add files via upload
    first

---
+++ sketch.js
@@ -0,0 +1,33 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var box1,box2;
+var ground;
+
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
+    box1= new Box(200,100,50,50);
+    box2= new Box(220,80,50,50);
+    ground=new Ground(200,390,800,20);
+   
+    console.log(ground.width);
+   
+
+    
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    ground.display();
+    box1.display();
+    box2.display();
+    
+
+
+   
+}
